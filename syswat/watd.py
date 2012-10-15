841238fe7000c6d3a44060389b62dc6b8be94ff6
whitmo <d.w.morriss@gmail.com>
2012-10-15 15:55:46 -0500

    improved the monitoring operation
    - heal handles emitter and collectors on a per failure basis
    
    - process is joined on the monitor now (which never should join, only
      exit or heal)

--- syswat/watd.py
+++ syswat/watd.py
@@ -24,22 +24,61 @@ class CarbonWatD(object):
     cli.add_argument("--cprefix_format", default="{toplevel}.{hostname}")
     cli.add_argument("--ctoplevel", default='node')
     cli.add_argument("--fauxcarbon", "-f", action="store_true", default=False)
+    cli.add_argument("--exit", "-x", action="store_true", default=False)
 
     emitter_class = carbon.Emitter
     syscol_class = carbon.SysCollector
     spidc_class = carbon.SuperPIDCollector
 
-    def __init__(self, mkcol=[], mkemit=None):
+    def __init__(self, mkcol=[], mkemit=None, exit_on_error=True):
         self.mkcol = mkcol
         self.mkemit = mkemit
         self.greenlets = []
+        self.mon_g = None
+        self.exit = exit_on_error
+        self.inbox = None
 
     def run(self):
+        self.start_colect_emit()
+        self.mon_g = gevent.spawn(self.monitor)
+        self.mon_g.join()
+
+    def start_colect_emit(self):
         e = self.mkemit()
-        self.greenlets.extend(cf(e.inbox) for cf in self.mkcol)
+        inbox = self.inbox = e.inbox
+        for cf in self.mkcol:
+            g = cf(inbox)
+            g._factory = cf
+            self.greenlets.append(g)
         self.greenlets.append(e)
         [g.start() for g in self.greenlets]
-        gevent.joinall(list(self.greenlets))
+
+    def monitor(self, interval=1.0):
+        """
+        A supervisoring daemon that runs in 2 modes (determined by
+        script input).
+
+        - heal: will attempt to restart any greenlet that had died
+        - exit: if any greenlet is dead, we bail
+        """
+        while True:
+            status = [x for x in self.greenlets if x.dead]
+            self.greenlets = [x for x in self.greenlets if not x.dead]
+            if any(x.dead for x in status):
+                if self.exit:
+                    logger.fatal("Died %s", status)
+                    sys.exit(1)
+                logger.error("Died: %s", status)
+                for dead in status:
+                    factory = getattr(dead, '_factory', None)
+                    if factory is None:
+                        new_g = self.mkemit()
+                    else:
+                        new_g = factory(self.inbox) 
+                        new_g._factory = dead._factory
+                    self.greenlets.append(new_g)
+                    new_g.start()
+            gevent.sleep(interval)
 
     def logging_setup(self, loglevel=logging.INFO):
         logging.basicConfig(level=loglevel,
@@ -73,7 +112,7 @@ class CarbonWatD(object):
 
         mk_emitter = partial(cls.emitter_class, args.chost, args.cport)
 
-        watd = cls([mk_syscol, mk_spidcol], mk_emitter)
+        watd = cls([mk_syscol, mk_spidcol], mk_emitter, args.exit)
         if args.fauxcarbon:
             logger.info("starting fake carbon server %s:%s", args.chost, args.cport)
             srv = cls.faux_carbon(args.cport)
